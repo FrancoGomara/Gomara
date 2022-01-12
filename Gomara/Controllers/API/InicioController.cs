dbdd6407f6bf67b2d1e0db0ae5d95e50861a0299
Franco Gomara <fgomara@aligare.cl>
2022-01-12 18:02:47 -0300

    first commit

---
+++ Gomara/Controllers/API/InicioController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gomara.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [Produces("application/json")]
+    public class InicioController : ControllerBase
+    {
+        // GET: api/v1/<InicioController>
+        [HttpGet]
+        public string Get()
+        {
+            return "Se Inicia el Proyecto Gomara.API";
+        }
+    }
+}
---
+++ Gomara/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Gomara.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
